1ac52782eeddf488c28dbdd9c464e893cb272c51
Daniela Rique <drique@asesoftware.com>
2023-08-11 15:13:10 -0500

    Inicio de curso

---
+++ src/main/java/com/platzi/mensajes_app/Mensajes_app.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.platzi.mensajes_app;
+
+/**
+ *
+ * @author Daniela
+ */
+public class Mensajes_app {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
