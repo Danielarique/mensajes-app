1c4792d88a93fa19bdfc70602532e7a385371e5a
Daniela Rique <drique@asesoftware.com>
2023-08-11 15:50:28 -0500

    Prueba

--- src/main/java/com/platzi/mensajes_app/Inicio.java
+++ src/main/java/com/platzi/mensajes_app/Inicio.java
@@ -14,7 +14,8 @@ public class Inicio {
     public static void main(String[] args) {
         Conexion conexion = new Conexion();
         try(Connection cnx= conexion.get_connection()){
-            
+                       System.out.println("Prueba");
+
         }catch(Exception e){
             System.out.println(e);
 
